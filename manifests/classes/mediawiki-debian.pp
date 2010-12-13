993e6e13ff1ff341e0fe9489df7133b53069e318
Carla Souza <crlnh.a@gmail.com>
2010-12-13 20:33:21 -0300

    Sintaxe fix

--- manifests/classes/mediawiki-debian.pp
+++ manifests/classes/mediawiki-debian.pp
@@ -9,20 +9,20 @@ class mediawiki::debian inherits mediawiki::base {
 		owner    => 'root',
 		group    => 'root',
 		ensure   => directory,
-		mode     => 755;
-		require	=> Package['mediawiki'] 
+		mode     => 755,
+		require	=> Package['mediawiki'];
 	}
 
 	file {
-		['/var/lib/mediawiki/wikis/resource[:name]/upload',
-		'/var/lib/mediawiki/wikis/resource[:name]/images',
-		'/var/lib/mediawiki/wikis/resource[:name]/config']:
+		['/var/lib/mediawiki/wikis/$resource[:name]/upload',
+		'/var/lib/mediawiki/wikis/$resource[:name]/images',
+		'/var/lib/mediawiki/wikis/$resource[:name]/config']:
 			owner		=> 'www-data',
 			group		=> 'www-data',
 			ensure	=> directory,
 			require	=> File['wikis'],
 			mode		=>	700;
-		'/var/lib/mediawiki/wikis/resource[:name]/extensions':
+		'/var/lib/mediawiki/wikis/$resource[:name]/extensions':
 			owner		=> 'root',
 			group		=> 'root',
 			ensure	=> directory,
@@ -31,48 +31,52 @@ class mediawiki::debian inherits mediawiki::base {
 	}
 	
 #acho q n precisa
-#	file {'/var/lib/mediawiki/wikis/resource[:name]/LocalSettings.php':
+#	file {'/var/lib/mediawiki/wikis/$resource[:name]/LocalSettings.php':
 #		content => template();
 #	}
+
+#     if ( file_exists( 'config/LocalSettings.php' ) ) {
+#         echo( 'To complete the installation, move <tt>config/LocalSettings.php</tt> to the parent directory.' );
+
 
 #		defaultsettings.php
 #		$wgUploadPath       = '{$wgScriptPath}/upload';
 #		$wgUploadDirectory      = '{$IP}/upload';
 
 	file {
-		'/var/lib/mediawiki/wikis/resource[:name]/api.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/api.php':
 			ensure => '/usr/share/mediawiki/api.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/img_auth.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/img_auth.php':
 			ensure => '/usr/share/mediawiki/img_auth.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/includes':
+		'/var/lib/mediawiki/wikis/$resource[:name]/includes':
 			ensure => '/usr/share/mediawiki/includes';
-		'/var/lib/mediawiki/wikis/resource[:name]/index.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/index.php':
 			ensure => '/usr/share/mediawiki/index.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/install-utils.inc':
+		'/var/lib/mediawiki/wikis/$resource[:name]/install-utils.inc':
 			ensure => '/usr/share/mediawiki/install-utils.inc';
-		'/var/lib/mediawiki/wikis/resource[:name]/languages':
+		'/var/lib/mediawiki/wikis/$resource[:name]/languages':
 			ensure => '/usr/share/mediawiki/languages';
-		'/var/lib/mediawiki/wikis/resource[:name]/maintenance':
+		'/var/lib/mediawiki/wikis/$resource[:name]/maintenance':
 			ensure => '/usr/share/mediawiki/maintenance';
-		'/var/lib/mediawiki/wikis/resource[:name]/opensearch_desc.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/opensearch_desc.php':
 			ensure => '/usr/share/mediawiki/opensearch_desc.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/profileinfo.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/profileinfo.php':
 			ensure => '/usr/share/mediawiki/profileinfo.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/redirect.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/redirect.php':
 			ensure => '/usr/share/mediawiki/redirect.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/redirect.phtml':
+		'/var/lib/mediawiki/wikis/$resource[:name]/redirect.phtml':
 			ensure => '/usr/share/mediawiki/redirect.phtml';
-		'/var/lib/mediawiki/wikis/resource[:name]/skins':
+		'/var/lib/mediawiki/wikis/$resource[:name]/skins':
 			ensure => '/usr/share/mediawiki/skins';
-		'/var/lib/mediawiki/wikis/resource[:name]/StartProfiler.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/StartProfiler.php':
 			ensure => '/usr/share/mediawiki/StartProfiler.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/Test.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/Test.php':
 			ensure => '/usr/share/mediawiki/Test.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/thumb.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/thumb.php':
 			ensure => '/usr/share/mediawiki/thumb.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/trackback.php':
+		'/var/lib/mediawiki/wikis/$resource[:name]/trackback.php':
 			ensure => '/usr/share/mediawiki/trackback.php';
-		'/var/lib/mediawiki/wikis/resource[:name]/wiki.phtml':
+		'/var/lib/mediawiki/wikis/$resource[:name]/wiki.phtml':
 			ensure => '/usr/share/mediawiki/wiki.phtml';
 	}
 	
@@ -89,8 +93,8 @@ class apache {
 		subscribe	=> File['apache-file'];
 	}
 
-	file {'apache-file'
-		path		=> '/etc/apache2/sites-enable/resource[:name]',
+	file {'apache-file':
+		path		=> '/etc/apache2/sites-enable/$resource[:name]',
 		content	=> template('wiki.erb');
 	}
 
--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,7 @@ Example usage:
 class mediawiki {
   case $operatingsystem {
     Debian,Ubuntu:  { include mediawiki::debian}
-    RedHat,CentOS:  { include mediawiki::redhat}
+/*    RedHat,CentOS:  { include mediawiki::redhat}
     default: { notice "Unsupported operatingsystem ${operatingsystem}" }
   }
 }
